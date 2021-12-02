2e044202496e76f102fd20bd6e7dee739b0c5f00
Anuj Kawane <akawane0813@sdsu.edu>
2021-12-01 18:46:12 -0800

    Graph Concepts

---
+++ src/Graphs/GraphDFS_BFS.java
@@ -0,0 +1,189 @@
+package Graphs;
+
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
+enum State {
+    UNVISITED,
+    VISITING,
+    VISITED
+}
+class Node{
+    List<Node> neighbors;
+    int data;
+    State state;
+
+    public Node(int data) {
+        this.neighbors = new ArrayList<>();
+        this.data = data;
+        this.state = State.UNVISITED;
+    }
+    public List<Node> getNeighbors() {
+        return neighbors;
+    }
+    public void addNeighbor(Node node) {
+        neighbors.add(node);
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+}
+
+class Graph{
+    List<Node> nodes;
+
+    public Graph() {
+        this.nodes = new ArrayList<>();
+    }
+
+    public void addNode(Node node){
+        this.nodes.add(node);
+    }
+
+    public List<Node> getNodes(){
+        return nodes;
+    }
+}
+
+public class GraphDFS_BFS {
+
+    /*
+    Find target node in Graph: return True if target present, false otherwise
+     */
+
+    public boolean findTargetUsingDFS(Graph graph, int target){
+        for(Node node : graph.getNodes()){
+            if(node.getState() == State.UNVISITED && findTargetUsingDFSHelper(node, target)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean findTargetUsingDFSHelper(Node node, int target){
+        node.setState(State.VISITING);
+        if(node.getData() == target)
+            return true;
+        for(Node neighbor : node.getNeighbors()){
+            if(neighbor.getState() == State.UNVISITED && findTargetUsingDFSHelper(neighbor, target))
+                return true;
+        }
+        node.setState(State.VISITED);
+        return false;
+    }
+
+    /*
+    Print DFS of graph
+     */
+
+    public void printDFS(Graph graph){
+        for(Node node : graph.getNodes()){
+            if(node.getState() == State.UNVISITED){
+                dfs(node);
+            }
+        }
+    }
+    public void dfs(Node start){
+        start.setState(State.VISITING);
+        System.out.print(start.getData()+" ");
+        for(Node neighbor : start.getNeighbors()){
+            if(neighbor.getState() == State.UNVISITED){
+                dfs(neighbor);
+            }
+        }
+        start.setState(State.VISITED);
+    }
+
+    public void printBFS(Graph graph){
+        for(Node node : graph.getNodes()){
+            if(node.getState() == State.UNVISITED){
+                bfs(node);
+            }
+        }
+    }
+
+    public void bfs(Node start){
+        Queue<Node> q = new LinkedList<>();
+        q.add(start);
+        while(!q.isEmpty()){
+            Node current = q.peek();
+            current.setState(State.VISITING);
+            System.out.print(current.getData()+" ");
+            for(Node neighbor : current.getNeighbors()){
+                if(neighbor.getState() == State.UNVISITED){
+                    q.add(neighbor);
+                }
+            }
+            q.peek().setState(State.VISITED);
+            q.remove();
+        }
+    }
+
+    public boolean hasCycle(Graph graph){
+        for(Node node : graph.getNodes()){
+            if(node.getState() ==  State.UNVISITED && detectCycleDFS(node)){
+                return true;
+            }
+        }
+        return false;
+    }
+    boolean detectCycleDFS(Node start){
+        start.setState(State.VISITING);
+        for(Node neighbor : start.getNeighbors()){
+            if(neighbor.getState() == State.UNVISITED && detectCycleDFS(neighbor))
+                return true;
+            if(neighbor.getState() == State.VISITING)
+                return true;
+        }
+        start.setState(State.VISITED);
+        return false;
+    }
+    public static void main(String[] args) {
+
+        Node a = new Node(1);
+        Node b = new Node(2);
+        Node c = new Node(3);
+        Node d = new Node(4);
+        Node e = new Node(5);
+        a.addNeighbor(b);
+        a.addNeighbor(c);
+        b.addNeighbor(e);
+        c.addNeighbor(b);
+        c.addNeighbor(d);
+        d.addNeighbor(e);
+//        e.addNeighbor(a); // create cycle e->a (5->1)
+        Graph g = new Graph();
+        g.addNode(a);
+        g.addNode(b);
+        g.addNode(c);
+        g.addNode(d);
+        g.addNode(e);
+
+        GraphDFS_BFS dfs = new GraphDFS_BFS();
+//        System.out.println(dfs.findTargetUsingDFS(g,6));
+//        dfs.printDFS(g);
+//        System.out.println();
+//        System.out.println("BFS below");
+//        Stack<Integer> stack = new Stack<>();
+//        stack.pop();
+//        dfs.printBFS(g);
+
+        System.out.println(dfs.hasCycle(g));
+
+    }
+
+}
