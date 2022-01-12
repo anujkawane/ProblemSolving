7809522cf0a6867059907264914b257b6f0f9267
Anuj Kawane <akawane0813@sdsu.edu>
2022-01-12 14:37:39 -0800

    Clone Even numbers in Array

---
+++ src/InterviewCampProblems/ArraysString/cloneEvenNumbers.java
@@ -0,0 +1,44 @@
+package InterviewCampProblems.ArraysString;
+/*
+(Level: Easy) Given an array of numbers, replace each even number with two of the same number.
+e.g, [1,2,5,6,8] -> [1,2,2,5,6,6,8,8]. Assume that the array has enough space to accommodate the result.
+ */
+public class cloneEvenNumbers {
+
+    public static void main(String[] args) {
+        int[] arr = new int[8];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 5;
+        arr[3] = 6;
+        arr[4] = 8;
+        arr[5] = -1;
+        arr[6] = -1;
+        arr[7] = -1;
+
+        int result[]  = clone(arr);
+        for(int n : result)
+            System.out.print(n+" ");
+
+    }
+
+    static int[] clone(int[] a) {
+        if (a == null || a.length == 0)
+            return a;
+        int end = a.length, i = getLastNumber(a);
+        while (i >= 0) {
+            if (a[i] % 2 == 0) {
+                a[--end] = a[i];
+            }
+            a[--end] = a[i];
+            i--;
+        }    return a;
+    }
+    public static int getLastNumber(int[] a) {
+        int i = a.length - 1;
+        while (i >= 0 && a[i] == -1) {
+            i--;
+        }
+        return i;
+    }
+}
